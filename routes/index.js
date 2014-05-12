d7b456c1b5f463e842d1da3af5cfa730c6337697
伯才 <xiaoqi.huxq@alibaba-inc.com>
2014-05-12 21:26:57 +0800

    add server.js

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ exports.index = function(req, res){
   res.render('index', { title: 'Express' });
 };
 
+
 exports.lib = function(req, res){
   res.render('lib', { title: 'Express' });
 };
---
+++ server.js
@@ -0,0 +1,90 @@
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var favicon = require('static-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var fs = require('fs');
+
+
+//glj add
+//var mongoSore = require('connect-mongo')(express);
+//var settings = require('./settings');
+
+var routes = require('./routes');
+// var users = require('./routes/user');
+
+var app = express();
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+
+
+// all environments  add glj
+app.set('port', process.env.PORT || 80);
+
+
+app.use(favicon());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+
+app.use(express.static(path.join(__dirname, 'public'))); //拒绝直接路径进行访问
+
+app.use(app.router);
+
+app.get('/', routes.index);
+app.get('/index.html', routes.index);
+app.get('/lib.html', routes.lib);
+//glj add 
+/*app.use(express.methodOverride());
+app.use(express.cookieParser());
+    app.use(express.session({
+    secret: settings.cookieSecret,
+    key: settings.db,
+    cookie: {maxAge: 1000 * 60 * 60 * 24 * 30},//30 days
+    store: new MongoStore({
+    db: settings.db
+    })
+}));*/
+
+
+/// catch 404 and forwarding to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+/// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+
+//下面是创建启动服务器：
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
+
+module.exports = app;
